dd9073525f294057fe4342037448ea47ffa92c69
tom <t.s.izumi@gmail.com>
2021-12-27 08:15:00 +0900

    finished almost 1h.

--- booking-app/main.go
+++ booking-app/main.go
@@ -13,12 +13,29 @@ func main() {
 	fmt.Printf("We have total of %v tikets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get yout tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTikets int
 	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
-	userTikets = 2
-	fmt.Printf("User %v booked  %v tickets.\n", userName, userTikets)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your number of tickets: ")
+	fmt.Scan(&userTikets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. you will confirmamtion email at %v \n", firstName, lastName, userTikets, email)
+
+	//	fmt.Println(remainingTickets)
+	//	fmt.Println(&remainingTickets)
+
+	//userTikets = 2
+	//fmt.Printf("User %v booked  %v tickets.\n", userName, userTikets)
 
 }
